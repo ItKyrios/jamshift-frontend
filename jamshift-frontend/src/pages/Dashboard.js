a65780183fe3644633eb8340987a298f93bd7352
Pramit Lama <kyriositsolutions@gmail.com>
2025-11-29 10:25:58 +1100

    First Page - Dashboard UI only

---
+++ jamshift-frontend/src/pages/Dashboard.js
@@ -0,0 +1,22 @@
+import Header from "../components/Header";
+import payslips_banner from "../assets/payslips_banner.png";
+import Navbar from "../components/Navbar";
+import StatsCard from "../components/StatsCard";
+import ShiftPreview from "../components/ShiftPreview";
+import Posts from "../components/Posts";
+
+export default function Dashboard() {
+  return (
+    <div>
+        <Header />
+        <Navbar />
+        <div className="stats">
+            <StatsCard title="Acces your Payslips" value={payslips_banner} />
+            <StatsCard title="Ideal Hours" value={idealhours_banner} />
+            <StatsCard title="Availability" value={availability_banner} />
+        </div>
+        <ShiftPreview />
+        <Posts />
+    </div>
+  );
+}
