5ca22220197262deb14f5835e12326b832c569ef
Pramit Lama <kyriositsolutions@gmail.com>
2025-11-30 11:26:29 +1100

    Minor UI fixes: border-radius, navbar, slider

--- jamshift-frontend/src/components/Navbar.js
+++ jamshift-frontend/src/components/Navbar.js
@@ -8,11 +8,11 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <ul>
-        <li><a href="/dashboard"><FaHome /></a></li>
-        <li><a href="/shifts"><FaCalendar /></a></li>
-        <li><a href="/messages"><MdMessage /></a></li>
-        <li><a href="/notifications"><FaBell /></a></li>
-        <li><a href="/profile"><TiThMenu /></a></li>
+        <li><a href="/dashboard"><FaHome /></a><span>Home</span></li>
+        <li><a href="/shifts"><FaCalendar /></a><span>Schedule</span></li>
+        <li><a href="/messages"><MdMessage /></a><span>Messages</span></li>
+        <li><a href="/notifications"><FaBell /></a><span>Notifications</span></li>
+        <li><a href="/more"><TiThMenu /></a><span>More</span></li>
       </ul>
     </nav>
   );
